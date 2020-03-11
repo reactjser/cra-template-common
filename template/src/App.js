92ff071e9402d6e879126dbdcb6e2418a43597e2
liuyao <reactjser@163.com>
2020-03-11 23:26:06 +0800

    test

--- template/src/App.js
+++ template/src/App.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import { HashRouter, NavLink } from 'react-router-dom';
-import Routes from './router';
 
 const App = () => {
-  return (
-    <HashRouter>
-      <div id="nav">
-        <NavLink exact to="/">
-          Home
-        </NavLink>{' '}
-        |{' '}
-        <NavLink exact to="/about">
-          About
-        </NavLink>
-      </div>
-      <Routes />
-    </HashRouter>
-  );
+  return <div>learn react</div>;
 };
 
 export default App;
